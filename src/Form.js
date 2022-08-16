400078b14efbd4edcc20bea3c43fd1ce3d48071d
pablop442 <89651989+pablop442@users.noreply.github.com>
2022-08-16 18:27:36 +0200

    added font style, colors and logo

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import FormInput from "./FormInput";
+import "./index.css";
 
 function Form() {
   const [investment, setInvestment] = useState("");
@@ -33,45 +34,56 @@ function Form() {
   };
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <FormInput text="CIRC.SUPPLY $MONSTA" placeholder="Enter amount" />
-      <FormInput text="$CAKE VAULT" placeholder="Enter amount" />
-      <FormInput text="$BNB VAULT" placeholder="Enter amount" />
-      <FormInput
-        text="INVESTMENT (USD)"
-        placeholder="Enter amount"
-        value={investment}
-        onKeyUp={calculatePurchased}
-        onInput={(e) => setInvestment(e.target.value)}
-      />
-      <FormInput
-        text="$MONSTA PRICE"
-        placeholder="Enter amount"
-        value={monstaPrice}
-        onKeyUp={calculatePurchased}
-        onInput={(e) => setMonstaPrice(e.target.value)}
-      />
-      <FormInput
-        text="$MONSTA PURCHASED"
-        readOnly={true}
-        value={monstaPurchased}
-      />
-      <FormInput
-        text="$MONSTA %"
-        placeholder="Enter percentage"
-        value={monstaPercentage}
-        onKeyUp={(e) => {
-          calculateMonstaBQuantity();
-          calculateMonstaQuantity();
-        }}
-        onInput={(e) => setMonstaPercentage(e.target.value)}
-      />
-      <FormInput text="$MONSTA" readOnly={true} value={monsta} />
-      <FormInput text="$BMONSTA" readOnly={true} value={monstaB} />
-      <button type="submit" className="btn btn-success btn-lg ">
-        Populate
-      </button>
-    </form>
+    <>
+      <div className="col-12 col-sm-6 d-flex justify-content-center m-auto">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div className="row">
+            <FormInput text="CIRC.SUPPLY $MONSTA" placeholder="Enter amount" />
+            <FormInput text="$CAKE VAULT" placeholder="Enter amount" />
+            <FormInput text="$BNB VAULT" placeholder="Enter amount" />
+          </div>
+          <div className="row ">
+            <FormInput
+              text="INVESTMENT (USD)"
+              placeholder="Enter amount"
+              value={investment}
+              onKeyUp={calculatePurchased}
+              onInput={(e) => setInvestment(e.target.value)}
+            />
+            <FormInput
+              text="$MONSTA PRICE"
+              placeholder="Enter amount"
+              value={monstaPrice}
+              onKeyUp={calculatePurchased}
+              onInput={(e) => setMonstaPrice(e.target.value)}
+            />
+            <FormInput
+              text="$MONSTA PURCHASED"
+              readOnly={true}
+              value={monstaPurchased}
+            />
+          </div>
+          <div className="row">
+            <FormInput
+              text="$MONSTA %"
+              placeholder="Enter percentage"
+              value={monstaPercentage}
+              onKeyUp={(e) => {
+                calculateMonstaBQuantity();
+                calculateMonstaQuantity();
+              }}
+              onInput={(e) => setMonstaPercentage(e.target.value)}
+            />
+            <FormInput text="$MONSTA" readOnly={true} value={monsta} />
+            <FormInput text="$BMONSTA" readOnly={true} value={monstaB} />
+          </div>
+
+          <button type="submit" className="btn clearbtn o btn-lg ">
+            Clear all
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -4,14 +4,19 @@ import Output from "./Output";
 import Form from "./Form";
 import { FcBullish } from "react-icons/fc";
 import "./index.css";
+import { ReactComponent as ReactLogo } from "../src/img/monster_head.svg";
 
 function Home() {
   return (
     <>
       <div className="container mt-3">
-        <h1 className="display-3 my-5">
-          ROI Calculator <FcBullish />{" "}
-        </h1>
+        <div className="row">
+          <ReactLogo />
+          <h1 className="display-5 my-3 d-flex justify-content-center title">
+            Cake Monster - ROI Calculator
+          </h1>
+        </div>
+
         <Form />
       </div>
     </>
