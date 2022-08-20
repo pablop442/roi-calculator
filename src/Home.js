cd47a4b1d0e7bad0119e76f6c305f2eb7056a780
pablop442 <89651989+pablop442@users.noreply.github.com>
2022-08-20 16:49:19 +0200

    changed top logo

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,23 @@
 import React from "react";
 import Form from "./Form";
+// import monstaLetters from "../src/img/MonstaLetters.jpeg";
 
 import "./index.css";
-import { ReactComponent as ReactLogo } from "../src/img/monster_head.svg";
+import { ReactComponent as ReactLogo } from "../src/img/Monster_Icon (3).svg";
 
 function Home() {
   return (
     <>
       <div className="container mt-3">
-        <div className="row">
-          <ReactLogo />
+        <div className="row d-flex justify-content-center">
+          <div className="col-4 d-flex justify-content-center">
+            <ReactLogo />
+          </div>
+
           <h1 className="display-5 my-3 d-flex justify-content-center title text-center">
             Cake Monster - ROI Calculator
           </h1>
         </div>
-
         <Form />
       </div>
     </>
